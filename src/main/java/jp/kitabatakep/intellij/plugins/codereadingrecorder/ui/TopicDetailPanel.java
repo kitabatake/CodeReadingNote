3207c83a19990571a26a5bf5250d7bfde32cff65
Tatsuya Kitabatake <e2tod2@gmail.com>
2020-03-15 17:09:49 +0900

    init TopicDetailPanel list

--- src/main/java/jp/kitabatakep/intellij/plugins/codereadingrecorder/ui/TopicDetailPanel.java
+++ src/main/java/jp/kitabatakep/intellij/plugins/codereadingrecorder/ui/TopicDetailPanel.java
@@ -1,21 +1,44 @@
 package jp.kitabatakep.intellij.plugins.codereadingrecorder.ui;
 
+import com.intellij.ui.JBSplitter;
+import com.intellij.ui.components.JBList;
+import jp.kitabatakep.intellij.plugins.codereadingrecorder.AppConstants;
 import jp.kitabatakep.intellij.plugins.codereadingrecorder.Topic;
+import jp.kitabatakep.intellij.plugins.codereadingrecorder.TopicLine;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Iterator;
 
-public class TopicDetailPanel extends JPanel
+class TopicDetailPanel extends JPanel
 {
     private final JLabel myLabel = new JLabel();
 
-    public TopicDetailPanel() {
+    private JBList<TopicLine> topicLineList;
+    private DefaultListModel<TopicLine> topicLineListModel;
+
+    public TopicDetailPanel()
+    {
         super(new BorderLayout());
-        add(myLabel);
+        topicLineList = new JBList<>();
+
+        JBSplitter splitPane = new JBSplitter(0.3f);
+        splitPane.setSplitterProportionKey(AppConstants.appName + "TopicDetailPanel.splitter");
+        splitPane.setFirstComponent(topicLineList);
+        splitPane.setSecondComponent(myLabel);
+        add(splitPane);
     }
 
     void setTopic(Topic topic)
     {
         myLabel.setText(topic.name());
+
+        topicLineListModel = new DefaultListModel<>();
+        Iterator<TopicLine> iterator = topic.getLinesIterator();
+        while (iterator.hasNext()) {
+            topicLineListModel.addElement(iterator.next());
+        }
+
+        topicLineList.setModel(topicLineListModel);
     }
 }
