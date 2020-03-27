f36571d7cadae34ff7eb2f7a9f840fe7d2436b82
Tatsuya Kitabatake <e2tod2@gmail.com>
2020-03-27 10:59:23 +0900

    check code

--- src/main/java/jp/kitabatakep/intellij/plugins/codereadingrecorder/CodeReadingRecorderService.java
+++ src/main/java/jp/kitabatakep/intellij/plugins/codereadingrecorder/CodeReadingRecorderService.java
@@ -1,13 +1,26 @@
 package jp.kitabatakep.intellij.plugins.codereadingrecorder;
 
+import com.github.hypfvieh.util.StringUtil;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.roots.libraries.LibraryUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import org.jdom.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.intellij.openapi.roots.libraries.LibraryUtil.getLibraryRoots;
 
 @State(
     name = AppConstants.appName,
@@ -24,6 +37,26 @@ public class CodeReadingRecorderService implements PersistentStateComponent<Elem
     {
         this.project = project;
         topicList = new TopicList(project);
+
+        Module[] modules = ModuleManager.getInstance(project).getModules();
+        String libNames = "";
+        for (Module module : modules) {
+           String moduleName = module.getName();
+
+            final List<String> libraryNames = new ArrayList<String>();
+            ModuleRootManager.getInstance(module).orderEntries().forEachLibrary(library -> {
+                libraryNames.add(library.getName());
+                return true;
+            });
+
+            libNames = StringUtil.join("\n", libraryNames);
+        }
+
+        VirtualFile[] libroots = LibraryUtil.getLibraryRoots(project);
+
+        Sdk projectSdk = ProjectRootManager.getInstance(project).getProjectSdk();
+        String projectSDKName = ProjectRootManager.getInstance(project).getProjectSdkName();
+        System.out.println(libNames);
     }
 
     public static CodeReadingRecorderService getInstance(@NotNull Project project)
